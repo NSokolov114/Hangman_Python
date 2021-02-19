3cb187c5f5e616667fa772b7e47eec5772de93fb
NSokolov114 <75481246+NSokolov114@users.noreply.github.com>
2021-02-20 00:18:15 +0300

    Update main.py
    small fixes

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ print(pictures.welcome_msg)
 
 # 2 choose the word (from the list in dictionary.py)
 the_word = '0'
-while len(the_word) < 10:                       # BONUS difficulty level - min length
-    the_word = random.choice(dictionary.words)  # chooses a new one
+while len(the_word) < 10:                       # difficulty level - setting min length
+    the_word = random.choice(dictionary.words)  # choose a word
     length = len(the_word)                      # calculates word's length
+                                                # repeat until the length is good
     
-# print(f"The word is: {the_word}, it's length is {len(the_word)}.")
 print(f'\nWelcome to the HANGMAN! Your word has {length} letters!\n')
 
 # 3 list made of _ _ _ _ _ using calculated length
@@ -31,14 +31,14 @@ for i in range (0,length):
     w_o_r_d.append(the_word[i])
 
 # 5 health, how many times a user can fail
-health = 6 # can't go over 6 w/o adding items to the list in pictures.py
+health = 6 # can't go over 6 w/o adding new pictures to the pictures.py
 
 # 6 creating the while loop
 guesses = []                                    # a list of all user inputs
 
 while health > 0 and '_' in dashes:             # breaks when health == 0 or the word is revealed
     guess = input('Please, choose a letter.\n') # input 
-    if guess not in dictionary.letters:         # check if input is correct, 1 English letter
+    if guess not in dictionary.letters:         # check if input is correct, 1 english letter
         print('Wrong input')                    # wrong input, proceed to losing health
     else:
         guess = guess.upper()                   # converting to UPPERcase
@@ -53,15 +53,15 @@ while health > 0 and '_' in dashes:             # breaks when health == 0 or the
         else:
             health -= 1                         # bad guess, health decreases
             print(f"You chose letter {guess}, that's not in the word. You lose a life.")    
-            print(pictures.gallows[health])     # printing gallows based on health
+            print(pictures.gallows[health])     # print gallows based on health
         print('')                               # empty line
         print(dashes_str)                       # _ X _ _ _
         print(f"\nYou checked the following letters: {guesses}") # showing which letters have been used
-if health <= 0:                                 # hangman is hanging
-    print(pictures.fail_msg)
+if health <= 0:                                 # GG, you're dead
+    print(pictures.fail_msg)                    # print GAME OVER
     print(f"\nSorry, you lost! Your word was '{the_word}'.")
-elif '_' not in dashes:                         # the word is revealed
-    print(pictures.win_msg)
+elif '_' not in dashes:                         # GZ, the word is revealed
+    print(pictures.win_msg)                     # print YOU WON
     print(f"\nCongratulations! Your word is '{the_word}'.")
 else:
-    print("If you see this, you broke the game")# in case something is wrong
+    print("If you see this, you broke the game")# just in case something is wrong
